f2c1d8724565a6352452e4752a78a5276a55e01f
Diego Alejandro Beltran <diegobeltran1016@hotmail.com>
2026-04-09 11:21:28 -0500

    docs(contract): adición de contrato operativo inicial del Doctor y scaffold mínimo tdc-doctor — m01-s04

---
+++ rust/doctor/target/debug/deps/tdc_doctor-ab6894ca1be40b75.d
@@ -0,0 +1,7 @@
+/repositorios/tiddly-data-converter/rust/doctor/target/debug/deps/tdc_doctor-ab6894ca1be40b75.d: src/lib.rs src/error.rs src/report.rs
+
+/repositorios/tiddly-data-converter/rust/doctor/target/debug/deps/tdc_doctor-ab6894ca1be40b75: src/lib.rs src/error.rs src/report.rs
+
+src/lib.rs:
+src/error.rs:
+src/report.rs:
